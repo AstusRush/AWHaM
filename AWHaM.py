a226b7c334e6727410643a4ed85a02435c07e54b
Robin 'Astus' Albers <a011235robin@gmail.com>
2024-05-06 15:55:45 +0200

    Initial Upload
    Basic git repo setup with minimal main file

---
+++ AWHaM.py
@@ -0,0 +1,8 @@
+from AGeLib import *
+
+class AWHaMWindow(AWWF):
+    def __init__(self):
+        super().__init__()
+
+if __name__ == "__main__":
+    AGeQuick.QuickSetup(AWHaMWindow, "Astus' TW:WH3 Mod Manager")
