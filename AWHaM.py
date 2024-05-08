257e4a736f71193621b28f8d7a14408f38bbbcda
Robin 'Astus' Albers <a011235robin@gmail.com>
2024-05-08 17:33:53 +0200

    ModSets Save/Load
    Modsets can now be saved and loaded.
    Mods that are not for warhammer 3 are now marked as such (as I suspect that the file that I use to manage the mods is used for all total war games which is quite insane but why else would there be that parameter. And the launcher that owns the file seems to be game independent)
    Improved and fixed some other things...

--- AWHaM.py
+++ AWHaM.py
@@ -27,7 +27,10 @@ class ModListItemWidget(AGeWidgets.TightGridWidget):
         #self.OrderInput.valueChanged.connect(lambda pos: self.reorder(pos))
         self.Picture = self.addWidget(ModLabel(),0,2)
         self.loadPicture()
-        self.NameLabel = self.addWidget(ModLabel(self.Name),0,3)
+        if self.Data["game"] == "warhammer3":
+            self.NameLabel = self.addWidget(ModLabel(self.Name),0,3)
+        else:
+            self.NameLabel = self.addWidget(ModLabel(self.Data["game"]+" mod: "+self.Name),0,3)
         self.ActiveCB = self.addWidget(QtWidgets.QCheckBox(""),0,1)
         self.ActiveCB.setFixedSize(20,20)
         self.ActiveCB.setChecked(self.Data["active"])
@@ -48,6 +51,10 @@ class ModListItemWidget(AGeWidgets.TightGridWidget):
     @Active.setter
     def Active(self, state:bool):
         self.ActiveCB.setChecked(state)
+    
+    @property
+    def ActiveStr(self) -> bool:
+        return "1" if self.ActiveCB.isChecked() else "0"
     
     def setOrderButBlockSignal(self, num):
         self.OrderInput.blockSignals(True)
@@ -190,18 +197,27 @@ class ModListWidget(AGeWidgets.ListWidget):
                 return True
         return False
     
-    def applyModOrderFromIDs(self, IDs):
+    def deactivateAll(self, shift=0):
         for _, _, w, _ in self.enumItems():
-            w.setOrderButBlockSignal(w.Order+len(IDs)+10) # +10 unnecessary but better save than sorry
+            if shift: w.setOrderButBlockSignal(w.Order+shift)
             w.Active = False
+    
+    def applyModOrderFromIDs(self, IDs, warnMissing=True):
+        """
+        Takes list of pairs as Steam WS ID and a str "1" or "0" for activation status.\n
+        Applies the mod order (order of entries in list) and activation status.\n
+        Moves all other mods below and deactivates them.\n
+        If warnMissing a warning notification is send to the user for each mod that is not found.
+        """
+        self.deactivateAll(len(IDs)+10) # +10 unnecessary but better save than sorry
         for i, ID in enumerate(IDs):
             for _, _, w, _ in self.enumItems():
-                if w.WorkshopID == ID:
+                if w.WorkshopID == ID[0]:
                     w.setOrderButBlockSignal(i+1)
-                    w.Active = True
+                    w.Active = ID[1] == "1"
                     break
             else:
-                NC(3,f"Mod with Steam Workshop ID {ID} was requested but could not be found! This should never happen! The other mods will still be sorted since the process has already started.")
+                if warnMissing: NC(1,f"Mod with Steam Workshop ID {ID} was requested but could not be found! This should never happen! The other mods will still be sorted since the process has already started.")
         self.sortItems()
 
 class AWHaMWindow(AWWF):
@@ -239,56 +255,112 @@ class AWHaMWindow(AWWF):
         self.ApplyButton = self.ModListButtonContainer.addWidget(AGeWidgets.Button(self,"Apply and Save",self.applyMods), 0,10)
         self.ShareButton = self.ModListButtonContainer.addWidget(AGeWidgets.Button(self,"Share",self.shareMods), 0,9)
         self.ShareButton.setToolTip("Copies a list of all active mods to the clipboard.\nPaste this into the chat of your choice so that your friends can copy it!")
+        self.ModSetSelect = self.ModListButtonContainer.addWidget(QtWidgets.QComboBox(self), 0,0)
         self.LoadFromClipboardButton = self.ModListButtonContainer.addWidget(AGeWidgets.Button(self,"Load from Clipboard",self.loadModsFromClipboard), 0,8)
         self.TabWidget.addTab(self.ModListContainer,"Mod List")
         
         self.start()
     
+    def start(self):
+        self.setupPaths()
+        self.ModListWidget.loadModList()
+        self.init_modSetSelect()
+    
+    def init_modSetSelect(self):
+        self.ModSetSelect.setEditable(False)
+        self.ModSetSelect.wheelEvent = lambda event: None
+        self.ModSetSelect.textActivated.connect(lambda text: self.modSetSelect(text))
+        self.loadModSetList()
+    
+    def loadModSetList(self, current="Select mod set to load..."):
+        self.ModSetSelect.clear()
+        self.ModSetSelect.addItems(["Select mod set to load...","Vanilla","SAVE AS NEW"])
+        files:typing.List[str] = [name.rstrip(".AWHaMSet") for name in os.listdir(self.ModSetStoragePath) if os.path.isfile(os.path.join(self.ModSetStoragePath, name))]
+        self.ModSetSelect.addItems(files)
+        self.ModSetSelect.setCurrentText(current)
+    
+    def modSetSelect(self, text:str):
+        if text=="Select mod set to load...":
+            return
+        elif text=="Vanilla":
+            self.ModListWidget.deactivateAll()
+        elif text=="SAVE AS NEW":
+            try:
+                name = QtWidgets.QInputDialog.getText(self,"Mod Set Name","What should the mod set be called?\n"\
+                                                        "NOTE: It must be a valid filename cause I am currently too lazy to implement a more complex system.\n"\
+                                                        "So only use ASCII numbers and letters and space and underscore to be on the save side.")[0].strip()
+                if name is None or name == "":
+                    NC(2,"saving mod set has been cancelled")
+                    return
+                modSetData = "TW:WH3 Mod List:"
+                for c, i, w, d in self.ModListWidget.enumItems():
+                    modSetData += f"\n{w.WorkshopID}:{w.ActiveStr}: {w.Name}"
+                with open(os.path.join(self.ModSetStoragePath, name+".AWHaMSet"),"w") as file:
+                    file.write(modSetData)
+            except:
+                NC(1,"Could not save modset!",exc=True)
+            else:
+                self.loadModSetList(name)
+        else:
+            try:
+                with open(os.path.join(self.ModSetStoragePath, text+".AWHaMSet"),"r") as file:
+                    self.loadMods(file.read(), skipMissing=False, skipNoID=True)
+            except:
+                NC(1,"Could not load modset!",exc=True)
+    
     def shareMods(self):
         try:
             s = "TW:WH3 Mod List:"
             for c, i, w, d in self.ModListWidget.enumItems():
-                if w.Active:
-                    s += f"\n{w.WorkshopID}: {w.Name}"
+                if w.Active and w.Data["game"] == "warhammer3":
+                    s += f"\n{w.WorkshopID}:{w.ActiveStr}: {w.Name}"
             QtWidgets.QApplication.clipboard().setText(s)
         except:
             NC(2,"Could not copy mod list to clipboard",exc=sys.exc_info())
         else:
-            NC(1,"Mods list is now in your clipboard!\nPaste this into the chat of your choice so that your friends can copy it!",DplStr="Now Paste into Chat!")
+            NC(3,"Mods list is now in your clipboard!\nPaste this into the chat of your choice so that your friends can copy it!",DplStr="Now Paste into Chat!")
     
     def loadModsFromClipboard(self):
+        s = QtWidgets.QApplication.clipboard().text()
+        if not s.startswith("TW:WH3 Mod List:\n"):
+            NC(2,"The content of the clipboard seems invalid.\nIt should start with \"TW:WH3 Mod List:\"",DplStr="Invalid Clipboard")
+            return
+        self.loadMods(s)
+    
+    def loadMods(self, text:str, skipMissing=False, skipNoID=False):
         try:
-            s = QtWidgets.QApplication.clipboard().text()
+            s:str = text
             if not s.startswith("TW:WH3 Mod List:\n"):
-                NC(2,"The content of the clipboard seems invalid.\nIt should start with \"TW:WH3 Mod List:\"",DplStr="Invalid Clipboard")
+                NC(2,"The content of the modset seems invalid.\nIt should start with \"TW:WH3 Mod List:\"",DplStr="Invalid modset")
                 return
             mods = s.splitlines()[1:]
             NotInstalled = []
             for i in mods:
+                if skipNoID and i.split(":")[0] == "N/A": continue
                 if not self.ModListWidget.checkIfModInstalled(i.split(":")[0]):
                     NotInstalled.append(i)
-            if NotInstalled:
+            if NotInstalled and not skipMissing:
                 NC(2,"The following mods are not installed, thus the modset can not be applied (Note: mods are searched by Steam Workshop ID):\n"+"\n".join(NotInstalled),DplStr="Mods Missing")
                 return
-            IDs = [i.split(":")[0] for i in mods]
+            IDs = [i.split(":")[0:2] for i in mods]
             self.ModListWidget.applyModOrderFromIDs(IDs)
             #for c, i, w, d in self.ModListWidget.enumItems():
             #    if w.Active:
             #        s += f"\n{w.WorkshopID}: {w.Name}"
             #QtWidgets.QApplication.clipboard().setText(s)
         except:
-            NC(2,"Could not apply mods",exc=sys.exc_info())
-    
-    def start(self):
-        self.setupPaths()
-        self.ModListWidget.loadModList()
+            NC(3,"Could not apply mods",exc=sys.exc_info())
     
     def applyMods(self):
         self.ModListWidget.applyMods()
         self.saveModFile(self.ModListWidget.ModData)
     
-    def setupPaths(self): #TODO: Add Windows support and dynamic selection
-        # Temp setup
+    def setupPaths(self):
+        self.AWHaMPath = os.path.join(App().AGeLibPath,"AWHaM")
+        os.makedirs(self.AWHaMPath,exist_ok=True)
+        self.ModSetStoragePath = os.path.join(self.AWHaMPath,"Mod Sets")
+        os.makedirs(self.ModSetStoragePath,exist_ok=True)
+        
         #self.WHModFileLastUsedLog = os.path.expanduser("~/.steam/steam/steamapps/common/Total War WARHAMMER III/used_mods.txt")
         if platform.system() == "Linux":
             self.WHModFile = os.path.expanduser("~/.steam/steam/steamapps/compatdata/1142710/pfx/drive_c/users/steamuser/AppData/Roaming/The Creative Assembly/Launcher")
